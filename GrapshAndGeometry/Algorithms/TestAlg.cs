75dd9191485893e01b2d60c28588f49b0d1f3fd8
Daniel Koprowski <dkoprowski@sigma.ug.edu.pl>
2016-03-09 17:19:09 +0100

    Added draft of menu workflow

---
+++ GrapshAndGeometry/Algorithms/TestAlg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrapshAndGeometry.Algorithms
+{
+    class TestAlg
+    {
+        public void Run()
+        {
+            Console.WriteLine("TEST ALGORITHM IS RUNNING!");
+        }
+    }
+}
--- GrapshAndGeometry/Program.cs
+++ GrapshAndGeometry/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GrapshAndGeometry.Algorithms;
 
 namespace GrapshAndGeometry
 {
@@ -10,7 +11,37 @@ namespace GrapshAndGeometry
     {
         static void Main(string[] args)
         {
-            Console.Write("Init");
+            WriteOptions();            
+        }
+
+        static void WriteOptions()
+        {
+            Console.WriteLine("\nChoose an algorithm:");
+            Console.WriteLine("Type [1] for Test");
+            Console.WriteLine("Type [exit] to finish program");
+
+            GetResponse();
+        }
+
+        static void GetResponse() 
+        {
+            string choosedOption = Console.ReadLine();
+            Console.WriteLine("-----------------------------------");
+
+            switch (choosedOption)
+            {
+                case "1":
+                    var test1 = new TestAlg();
+                    test1.Run();
+                    break;
+                default:
+                    Console.WriteLine("I don't understand. Try something from list below: ");
+                    break;
+                case "exit":
+                    return;
+            }
+            Console.WriteLine("-----------------------------------");
+            WriteOptions();
         }
     }
 }
