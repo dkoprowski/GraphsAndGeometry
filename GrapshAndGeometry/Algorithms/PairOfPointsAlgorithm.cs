1941fb52a25895b354c4bb3773a3c834dc4cd165
Daniel Koprowski <dkoprowski@outlook.com>
2016-03-13 21:04:04 +0100

    #1 sorting rule for points & dividing to two sets

--- GrapshAndGeometry/Algorithms/PairOfPointsAlgorithm.cs
+++ GrapshAndGeometry/Algorithms/PairOfPointsAlgorithm.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GrapshAndGeometry.Algorithms
 {
-    class Point
+    class Point : IComparable<Point>
     {
         public int x;
         public int y;
@@ -16,12 +16,54 @@ namespace GrapshAndGeometry.Algorithms
             this.x = x;
             this.y = y;
         }
+
+        int IComparable<Point>.CompareTo(Point other)
+        {
+            if (x < other.x)
+            {
+                return -1;
+            }
+            else if (x > other.x)
+            {
+                return 1;
+            }
+            else if (y < other.y)
+            {
+                return -1;
+            }
+            else if (y > other.y)
+            {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "(" + x.ToString() + "," + y.ToString() + ")";
+        }
+
+        public float DistanceTo(Point other)
+        {
+            return (float)Math.Sqrt(Math.Pow((x-other.x),2)+ Math.Pow(y-other.y,2));
+        }
     }
     class PairOfPointsAlgorithm : IAlgorithm
     {
         public void Run()
         {
             var points = LoadPoints();
+            points.Sort();
+
+            var lowerPointsSet = points.GetRange(0, points.Count / 2);
+            var higherPointsSet = points.GetRange((points.Count / 2), points.Count - lowerPointsSet.Count);
+
+            lowerPointsSet.ForEach(point => Console.WriteLine(point.ToString()));
+            higherPointsSet.ForEach(point => Console.WriteLine(point.ToString()));
+
+            int dividingStraight = lowerPointsSet[lowerPointsSet.Count - 1].x;
 
         }
 
@@ -31,13 +73,13 @@ namespace GrapshAndGeometry.Algorithms
             {
                 new Point(-3,0),
                 new Point(-2,-2),
+                new Point(3,3),
+                new Point(4,2),
+                new Point(5,5),
                 new Point(-1,-4),
                 new Point(0,-1),
                 new Point(1,1),
                 new Point(2,-3),
-                new Point(3,3),
-                new Point(4,2),
-                new Point(5,5),
                 new Point(6,4)
             };
         }
