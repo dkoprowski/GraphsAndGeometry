5fb959c0c540fe95f91afe341dc23d252e85c019
Daniel Koprowski <dkoprowski@outlook.com>
2016-04-12 21:43:37 +0200

    exctratet Point class to file

---
+++ GrapshAndGeometry/Algorithms/Point.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrapshAndGeometry.Algorithms
+{
+    class Point : IComparable<Point>
+    {
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        int IComparable<Point>.CompareTo(Point other)
+        {
+            if (x < other.x)
+            {
+                return -1;
+            }
+            else if (x > other.x)
+            {
+                return 1;
+            }
+            else if (y < other.y)
+            {
+                return -1;
+            }
+            else if (y > other.y)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "(" + x.ToString() + "," + y.ToString() + ")";
+        }
+
+        public float DistanceTo(Point other)
+        {
+            return (float)Math.Sqrt(Math.Pow((x - other.x), 2) + Math.Pow(y - other.y, 2));
+        }
+    }
+}
