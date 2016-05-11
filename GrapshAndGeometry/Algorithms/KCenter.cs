6ea045a804908d8e29a41c0e96e3f636025b9350
Daniel Koprowski <dkoprowski@outlook.com>
2016-05-11 20:53:31 +0200

    k-center problem almost working#4

--- GrapshAndGeometry/Algorithms/KCenter.cs
+++ GrapshAndGeometry/Algorithms/KCenter.cs
@@ -11,21 +11,64 @@ namespace GrapshAndGeometry.Algorithms
         public void Run()
         {
             var settlements = GetSettlements();
+            FurthestFirst(settlements, 3);
+
+
         }
 
-        private List<Point> GetSettlements()
+        private void FurthestFirst(Dictionary<Point, float> settlements, int centresCount)
         {
-            return new List<Point>()
+            var hospitals = new Dictionary<Point, float>();
+            hospitals.Add(settlements.First().Key, settlements.First().Value);
+
+            for (int i = 1; i < centresCount; i++)
             {
-                new Point(1,1),
-                new Point(1,7),
-                new Point(3,2),
-                new Point(3,5),
-                new Point(5,7),
-                new Point(6,3),
-                new Point(7,5),
-                new Point(7,2),
-                new Point(10,1)
+                var FarestSettlement = UpdateDistanceToHospital(settlements, hospitals.Last().Key);
+                hospitals.Add(FarestSettlement.Key, FarestSettlement.Value);
+            }
+
+            foreach (var item in hospitals)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+
+        private KeyValuePair<Point, float> UpdateDistanceToHospital(Dictionary<Point, float> settlements, Point hospital)
+        {
+            float longestDistance = float.NegativeInfinity;
+            Point farestSettlement = null;
+            foreach (var item in settlements.Keys.ToList())
+            {
+                float distance = hospital.DistanceTo(item);
+
+                if (settlements[item] < distance)
+                {
+                    settlements[item] = distance;                   
+                }
+
+                if (settlements[item] > longestDistance)
+                {
+                    farestSettlement = item;
+                    longestDistance = settlements[item];
+                }
+            }
+
+            return new KeyValuePair<Point, float>(farestSettlement, longestDistance);
+        }
+
+        private Dictionary<Point, float> GetSettlements()
+        {
+            return new Dictionary<Point, float>()
+            {
+                {new Point(1,1), float.NegativeInfinity},
+                {new Point(1,7), float.NegativeInfinity},
+                {new Point(3,2), float.NegativeInfinity},
+                {new Point(3,5), float.NegativeInfinity},
+                {new Point(5,7), float.NegativeInfinity},
+                {new Point(6,3), float.NegativeInfinity},
+                {new Point(7,5), float.NegativeInfinity},
+                {new Point(7,2), float.NegativeInfinity},
+                {new Point(10,1), float.NegativeInfinity}
             };
         }
     }
